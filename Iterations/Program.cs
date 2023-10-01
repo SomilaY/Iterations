bc62367baf910151fb9282e654ce5abe52147c03
SomilaY <ST10091012@vcconnect.edu.za>
2023-10-01 10:34:41 +0000

    Add project files.

---
+++ Iterations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========");
+            Console.ReadLine();
+            Print(7);
+
+          
+            
+        }
+
+        static void Print(int x)
+        {
+            if (x <= 10)
+            {
+                Console.WriteLine("Hello World");
+                Print(x + 1);
+            }
+        }
+    }
+}
